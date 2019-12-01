f2d9300a2d712480bc4465303ec523355561008a
Michael Angerman <stormasm@gmail.com>
2019-11-30 21:07:22 -0500

    mw

--- examples/json_de_value.rs
+++ examples/json_de_value.rs
@@ -3,7 +3,7 @@ use hn_api::HnClient;
 
 fn print(api: &HnClient, items: &[u32]) {
     for id in items {
-        println!("id = {}", id);
+        println!("\nid = {}\n", id);
 
         let item = api.get_json(*id).unwrap();
         println!("{:?}", item);
@@ -15,7 +15,7 @@ fn print(api: &HnClient, items: &[u32]) {
 
 fn main() {
     let api = HnClient::init().unwrap();
-    let top = [21669726];
-    let count = 1;
+    let top = [21669726, 21672696];
+    let count = 2;
     print(&api, &top[..count]);
 }
