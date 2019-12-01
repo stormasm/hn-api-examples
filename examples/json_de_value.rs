c3af8ba071b0baef81af1ac755a705b8dd3ec896
Michael Angerman <stormasm@gmail.com>
2019-11-30 21:22:24 -0500

    we are now checking for nulls in the hackernews api and handling the null and moving on to the next item, this is great news and a big local breakthrough

--- examples/json_de_value.rs
+++ examples/json_de_value.rs
@@ -1,3 +1,5 @@
+/// Checks for null in the hackernews feed
+/// If a null is found, do not parse and keep going...
 use hn_api::types::Story;
 use hn_api::HnClient;
 
@@ -6,16 +8,23 @@ fn print(api: &HnClient, items: &[u32]) {
         println!("\nid = {}\n", id);
 
         let item = api.get_json(*id).unwrap();
-        println!("{:?}", item);
 
-        let story: Story = serde_json::from_str(&item).unwrap();
-        println!("\n{:?} {:?}", story.by.unwrap(), story.title.unwrap());
+        match item.as_ref() {
+            "null" => println!("null for id = {}", id),
+            _ => {
+                println!("{:?}", item);
+                let story: Story = serde_json::from_str(&item).unwrap();
+                println!("\n{:?} {:?}", story.by.unwrap(), story.title.unwrap());
+            }
+        }
     }
 }
 
 fn main() {
     let api = HnClient::init().unwrap();
-    let top = [21669726, 21672696];
-    let count = 2;
+    let top = [
+        21669726, 21664126, 21672696, 21655779, 21672563, 21655225, 21669234,
+    ];
+    let count = 7;
     print(&api, &top[..count]);
 }
