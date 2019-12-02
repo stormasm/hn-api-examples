f2a6afbb0f1bed572ab811efce4156727930b7a0
Michael Angerman <stormasm@gmail.com>
2019-12-02 08:06:03 -0500

    finished porting over code to a working null check with item instead of story

--- examples/top_n_items.rs
+++ examples/top_n_items.rs
@@ -1,6 +1,26 @@
+use hn_api::types::Item;
 use hn_api::HnClient;
 
 fn process_items(api: &HnClient, item_ids: Vec<u32>) {
+    for item_id in item_ids {
+        // println!("\nid = {}\n", item_id);
+
+        let item_json = api.get_json(item_id).unwrap();
+
+        match item_json.as_ref() {
+            "null" => println!("{} null", item_id),
+            _ => {
+                // println!("{:?}", item_id);
+                let item: Item = serde_json::from_str(&item_json).unwrap();
+                println!("{} {}", item_id, item.item_type());
+                // println!("\n{:?} {:?}", story.by.unwrap(), story.title.unwrap());
+            }
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn process_items_old(api: &HnClient, item_ids: Vec<u32>) {
     for item_id in &item_ids {
         let item = api.get_item(*item_id).unwrap().unwrap();
         println!("{} {}", item_id, item.item_type());
@@ -22,9 +42,9 @@ fn top_n_items(numofitems: u32, max_number: u32) -> Vec<u32> {
 fn main() {
     let api = HnClient::init().unwrap();
 
-    // let max_item_id = api.get_max_item_id().unwrap();
+    let max_item_id = api.get_max_item_id().unwrap();
     // Instead of the max_item_id start with this item_id
-    let max_item_id = 21625360;
+    // let max_item_id = 21625360;
     println!("max item id = {}", max_item_id);
 
     let item_ids = top_n_items(10000, max_item_id);
