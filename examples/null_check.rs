6a79896ff7c42bc69269768a2e8ce2434aa993b6
Michael Angerman <stormasm@gmail.com>
2019-12-01 21:22:21 -0500

    switch to vec as an input parameter

--- examples/null_check.rs
+++ examples/null_check.rs
@@ -3,11 +3,11 @@
 use hn_api::types::Story;
 use hn_api::HnClient;
 
-fn nullcheck(api: &HnClient, items: &[u32]) {
+fn nullcheck(api: &HnClient, items: Vec<u32>) {
     for id in items {
         println!("\nid = {}\n", id);
 
-        let item = api.get_json(*id).unwrap();
+        let item = api.get_json(id).unwrap();
 
         match item.as_ref() {
             "null" => println!("null for id = {}", id),
@@ -22,9 +22,8 @@ fn nullcheck(api: &HnClient, items: &[u32]) {
 
 fn main() {
     let api = HnClient::init().unwrap();
-    let top = [
+    let top = vec![
         21669726, 21664126, 21672696, 21655779, 21672563, 21655225, 21669234,
     ];
-    let count = 7;
-    nullcheck(&api, &top[..count]);
+    nullcheck(&api, top);
 }
