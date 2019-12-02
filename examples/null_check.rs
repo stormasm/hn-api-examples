ee38b1ca9b23c8238364c211ab73f30a1a1c3781
Michael Angerman <stormasm@gmail.com>
2019-12-02 07:56:27 -0500

    reformat for future processing

--- examples/null_check.rs
+++ examples/null_check.rs
@@ -5,14 +5,14 @@ use hn_api::HnClient;
 
 fn nullcheck(api: &HnClient, item_ids: Vec<u32>) {
     for item_id in item_ids {
-        println!("\nid = {}\n", item_id);
+        // println!("\nid = {}\n", item_id);
 
         let item_json = api.get_json(item_id).unwrap();
 
         match item_json.as_ref() {
-            "null" => println!("null for id = {}", item_id),
+            "null" => println!("{} null", item_id),
             _ => {
-                println!("{:?}", item_id);
+                // println!("{:?}", item_id);
                 let item: Item = serde_json::from_str(&item_json).unwrap();
                 println!("{} {}", item_id, item.item_type());
                 // println!("\n{:?} {:?}", story.by.unwrap(), story.title.unwrap());
