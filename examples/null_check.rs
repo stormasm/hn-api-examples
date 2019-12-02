7504b0a04cfc0ecbd68baa896e5c308f4a53bc7e
Michael Angerman <stormasm@gmail.com>
2019-12-02 07:53:52 -0500

    changing from story to item for conversion

--- examples/null_check.rs
+++ examples/null_check.rs
@@ -1,20 +1,21 @@
 /// Checks for null in the hackernews feed
 /// If a null is found, do not parse and keep going...
-use hn_api::types::Story;
+use hn_api::types::Item;
 use hn_api::HnClient;
 
 fn nullcheck(api: &HnClient, item_ids: Vec<u32>) {
-    for id in item_ids {
-        println!("\nid = {}\n", id);
+    for item_id in item_ids {
+        println!("\nid = {}\n", item_id);
 
-        let item = api.get_json(id).unwrap();
+        let item_json = api.get_json(item_id).unwrap();
 
-        match item.as_ref() {
-            "null" => println!("null for id = {}", id),
+        match item_json.as_ref() {
+            "null" => println!("null for id = {}", item_id),
             _ => {
-                println!("{:?}", item);
-                let story: Story = serde_json::from_str(&item).unwrap();
-                println!("\n{:?} {:?}", story.by.unwrap(), story.title.unwrap());
+                println!("{:?}", item_id);
+                let item: Item = serde_json::from_str(&item_json).unwrap();
+                println!("{} {}", item_id, item.item_type());
+                // println!("\n{:?} {:?}", story.by.unwrap(), story.title.unwrap());
             }
         }
     }
