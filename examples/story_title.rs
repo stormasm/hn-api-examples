8d44d36a47fb56c668808f7cbf7f2d54cdc92025
Michael Angerman <stormasm@gmail.com>
2020-01-23 16:45:42 -0800

    cc

--- examples/story_title.rs
+++ examples/story_title.rs
@@ -1,12 +1,9 @@
 use hn_api::types::Item;
-
 use r2d2_redis::{r2d2, RedisConnectionManager};
 use redis::{Commands, RedisResult};
-
+use serde_json::json;
 use std::fs::File;
 use std::io::{Error, Write};
-
-use serde_json::json;
 
 fn get_hashmap_keys(key: String) -> RedisResult<Vec<u32>> {
     let manager = RedisConnectionManager::new("redis://localhost").unwrap();
@@ -36,12 +33,9 @@ fn main() -> Result<(), Error> {
         let value: RedisResult<String> = con.hget("hn-story-20".to_string(), key.to_string());
         let item_json = value.unwrap();
         let item: Item = serde_json::from_str(&item_json).unwrap();
-        // let story_title = &item.title().unwrap();
         let story_title = &item.title().unwrap();
 
         if story_title != &no_title {
-            // println! {"{}", story_title};
-
             let title_json = json!({
                 "id": key,
                 "title": story_title,
@@ -56,37 +50,3 @@ fn main() -> Result<(), Error> {
     println!("Number of keys = {}", keys.len());
     Ok(())
 }
-
-/*
-        match item_json.as_ref() {
-            "null" => println!("\n{} null\n", key),
-            _ => {
-                let item: Item = serde_json::from_str(&item_json).unwrap();
-                let item_type = item.item_type();
-                match item_type.as_ref() {
-                    "story" => {
-                        // println!("{} story", key);
-                        println!("{}, {:?}", key, item.title().unwrap());
-                        // let _ = write_json_to_redis(item_id.to_string(), item_json);
-                    }
-                    _ => {}
-                }
-            }
-        }
-*/
-//        println!("{:?}", item.title().unwrap());
-
-/*
-        println!("{} story", item_id);
-        println!("{:?}", item.title().unwrap());
-
-        let titlejson = json!({
-            "id": key,
-            "title": title,
-        });
-
-        // Convert to a string of JSON and print it out
-        println!("{}", titlejson.to_string());
-*/
-//write!(output, "{}", item_json.to_string())?;
-//write!(output, "{}", "\n")?;
